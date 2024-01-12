2ba3a784c83027dda2114fcc38ade27c0319af7f
antonioojosep <antonillocarriillo@gmail.com>
2024-01-12 13:36:47 +0100

    Ejercicios

---
+++ Ejercicios/src/Components/UseState/Onclick.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Onclick = () => {
+  return (
+    <button className='Boton'
+    onClick={() => (console.log("Boton presionado"))}>Pulsa este botón</button>
+  )
+}
+
+export default Onclick;
---
+++ Ejercicios/src/Components/UseState/UseList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const nombres = ["Antonio" , "Pepe", "Jose", "Maria", "Laura"];
+
+const UseList = () => {
+
+    function handleSearchName(array) {
+        array.map((nombre) => )
+    }
+
+  return (
+    <div>UseList</div>
+  )
+}
+
+export default UseList;
