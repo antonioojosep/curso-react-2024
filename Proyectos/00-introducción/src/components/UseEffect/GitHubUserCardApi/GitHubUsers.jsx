a3a744037e8418387dacf2e7b548bc1bb8285a65
antonioojosep <antonillocarriillo@gmail.com>
2024-01-18 13:44:25 +0100

    18-01-2024

--- Proyectos/00-introducción/src/components/UseEffect/GitHubUserCardApi/GitHubUsers.jsx
+++ Proyectos/00-introducción/src/components/UseEffect/GitHubUserCardApi/GitHubUsers.jsx
@@ -4,11 +4,22 @@ import { useState } from "react";
 import fetchGitHubUsers from "../../../helpers/fetchGitHubUsers";
 import CardGitHub from "./CardGitHub";
 import Spinner from "./Spinner";
+import Modal from "./Modal";
 
 const GitHubUsers = () => {
   // hooks
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const handleOpenModal = (user) => {
+    setSelectedUser(user);
+  }
+
+  const handleCloseModal = () => {
+    setSelectedUser(null);
+  }
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +30,7 @@ const GitHubUsers = () => {
         setUsers(data);
         setTimeout(() => {
           setLoading(false);
-        }, 5000);
+        }, 3000);
         
       } catch (error) {
         setLoading(true);
@@ -32,19 +43,23 @@ const GitHubUsers = () => {
   return (
     <>
         <div className="mx-auto max-w-[75%] text-center relative">
-            <h1 className="text-4xl font-bold mb-8">
+            <h1 className="text-4xl font-bold mb-8 mt-2">
                 Usando UseEffect para realizar un FETCH  de la API GitHub
             </h1>
-            
             {loading? (<Spinner/>) : (
-             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grap-4 mx-auto relative">
+              <>
+             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto relative">
                 {users.map(user => (
                     <CardGitHub key={user.id}
                     avatar_url={user.avatar_url}
                     login={user.login}
-                    html_url={user.html_url}/>
+                    html_url={user.html_url}
+                    openModal={() => handleOpenModal(user)}
+                    />
                 ))}
              </div>
+              <Modal isOpen={!!selectedUser} imgUrl={selectedUser?.avatar_url} closeModal={handleCloseModal}/>
+             </>
             )}
         </div>
     </>
